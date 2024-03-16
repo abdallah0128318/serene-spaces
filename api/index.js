19477b199702cfc0ad7df9e23f07009082c76ed5
abdallah0128318 <boda0128318@gmail.com>
2024-03-16 22:39:40 +0200

    conneted to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
-const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+// initialize 'dotenv' package
+dotenv.config()
+// create an express app
+const app = express()
+// connect to mongodb then listen to requests
+mongoose.connect(process.env.MONGO_CONN_STR).then(data => {
+    app.listen(3000, ()=>{
+        console.log('server starts at port 3000');
+    })
+}).catch(err => console.log(err))
 
-app.listen(3000, ()=>{
-    console.log('server starts at port 3000');
-})
+
